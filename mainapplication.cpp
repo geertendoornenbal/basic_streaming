bcca0ef0c267194e384b482d609a54475fab1442
Geerten Doornenbal <geertendoornenbal@gmail.com>
2015-10-10 17:53:18 +0200

    added class for main application management

---
+++ mainapplication.cpp
@@ -0,0 +1,5 @@
+#include "mainapplication.h"
+
+MainApplication::MainApplication()
+{
+}
---
+++ mainapplication.h
@@ -0,0 +1,10 @@
+#ifndef MAINAPPLICATION_H
+#define MAINAPPLICATION_H
+
+class MainApplication
+{
+public:
+    MainApplication();
+};
+
+#endif // MAINAPPLICATION_H
